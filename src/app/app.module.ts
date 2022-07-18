7bd2a433ac6f75f78d4478c22a11ad8e16232354
Avcharov <avchargrisha@gmail.com>
2022-07-18 11:10:03 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { Car } from './car/car.component';
-import { Cars } from './cars/cars.component';
-import { AddCar} from './add-car/add-car.component';
+
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, Car, Cars,AddCar],
+  declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
